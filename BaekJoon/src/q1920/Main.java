4602fc675811b297dc32c80a2f948cc06c67665e
21-manic-010g <21-manic-010g@21-manic-010>
2024-05-30 17:46:32 +0900

    123456

--- BaekJoon/src/q1920/Main.java
+++ BaekJoon/src/q1920/Main.java
@@ -1,15 +1,14 @@
 package q1920;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args)throws IOException {
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int input = Integer.parseInt(bf.readLine());
 		String a[] = bf.readLine().split(" ");
@@ -25,7 +24,17 @@ public class Main {
 			inputnums2[i] = Integer.parseInt(b[i]);
 		}
 		
+//		int input3[] = new int[input2];
 		
+		Arrays.sort(inputnums);
+		
+		for(int i = 0; i < input2 ; i++) {
+			if(0 <= Arrays.binarySearch(inputnums, inputnums2[i])) {
+				System.out.println(1 + " ");
+			}else {
+				System.out.println(0 + " ");
+			}
+		}
 	}
 
 }
